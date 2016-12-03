3338de4d1cf188aa9e721e6da1f406e9a7345c15
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-12-03 19:29:28 +0000

    Initial commit
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

---
+++ README.rst
@@ -0,0 +1,13 @@
+oidc-register
+=============
+
+`OpenID Connect <https://openid.net/connect/>`_ Dynamic Client Registration tool.
+
+.. image:: https://img.shields.io/pypi/v/oidc-register.svg?style=flat
+  :target: https://pypi.python.org/pypi/oidc-register
+
+This utility should work with any standards compliant OpenID Connect provider.
+
+It has been tested with:
+
+* `Ipsilon <https://ipsilon-project.org/>`_
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os.path
+import io
+import sys
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name='oidc-register',
+    description='OpenID Connect Dynamic Client Registration tool',
+    long_description=readme,
+    url='https://github.com/puiterwijk/oidc-register',
+    author='Patrick Uiterwijk',
+    author_email='patrick@puiterwijk.org',
+    version='0.0.1',
+    packages=[
+        'oidc_register',
+    ],
+    install_requires=[
+        'six',
+    ],
+    tests_require=['nose', 'mock'],
+    entry_points={
+        'console_scripts': ['oidc-register=oidc_register.registration_util:main'],
+    },
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
